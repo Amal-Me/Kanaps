3c16420662be46e118f9e98b7dd789d29e7b05b7
amel <amel.lys@hotmail.fr>
2021-11-06 18:11:01 +0100

    gestion error

--- P5-Dev-Web-Kanap/front/js/module.js
+++ P5-Dev-Web-Kanap/front/js/module.js
@@ -2,9 +2,10 @@ export const urlApi = `http://localhost:3000/api/products`;
 // recuperation produits API
 export function fetchApi(a,b) { 
     return new Promise((fonction) => { // retourne une promesse a la fonction 
-        fetch (a,b).then(res => { // appel API 
+       fetch (a,b).then(res => { // appel API 
             if (res.ok) { return res.json()}  // si obtenu, reour au format JSON 
         })
         .then(result => fonction(result))// renvoi le resultat dans cette fonction en argument
-    })
-}
+        .catch(result => alert("HTTP-Error: " + result.status))
+    }
+    )}
